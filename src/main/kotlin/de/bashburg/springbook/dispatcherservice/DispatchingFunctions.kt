a0cedaa817d1619836e2d7c5b671dfbe98753f99
Christian Ambach <christian.ambach@codecentric.de>
2024-08-16 16:11:59 +0200

    convert to kotlin lambda syntax

--- src/main/kotlin/de/bashburg/springbook/dispatcherservice/DispatchingFunctions.kt
+++ src/main/kotlin/de/bashburg/springbook/dispatcherservice/DispatchingFunctions.kt
@@ -4,7 +4,6 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import reactor.core.publisher.Flux
-import java.util.function.Function
 
 private val logger = KotlinLogging.logger {}
 
@@ -12,16 +11,14 @@ private val logger = KotlinLogging.logger {}
 class DispatchingFunctions {
 
     @Bean
-    fun pack() =
-        Function<OrderAcceptedMessage, Long> { orderAcceptedMessage ->
-            logger.info { "The order with id ${orderAcceptedMessage.orderId} is packed" }
-            orderAcceptedMessage.orderId
-
-        }
+    fun pack(): (OrderAcceptedMessage) -> Long = { orderAcceptedMessage ->
+        logger.info { "The order with id ${orderAcceptedMessage.orderId} is packed" }
+        orderAcceptedMessage.orderId
+    }
 
     @Bean
-    fun label() =
-        Function<Flux<Long>, Flux<OrderDispatchedMessage>> { orderFlux ->
+    fun label(): (Flux<Long>) -> Flux<OrderDispatchedMessage> =
+        { orderFlux ->
             orderFlux.map { orderId ->
                 logger.info { "The order with id $orderId is labelled" }
                 (OrderDispatchedMessage(orderId))
